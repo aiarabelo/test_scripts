fa09c982977b2da6d0f01275a2b81df04c3d8ebc
Allison Arabelo <aiarabelo@up.edu.ph>
2020-02-06 21:16:14 -0500

    Fixed format_type classification

--- edit_poscar_working.py
+++ edit_poscar_working.py
@@ -10,38 +10,48 @@ for i in range(0,7):
 
 selective_dynamics = input("Turn on Selective Dynamics? [y/n]: ")
 
-format_type = None
-regex = None
-
 atomic_species = f_read[5].split( )
 number_of_atoms = f_read[6].split( )
 
 def check_format():
-	## TODO: Fix this. does not recognize types 1 and 2 
-	regex = r"(![a-zA-Z]+)+"
-	r = re.search(regex, f_read[8])
-
 	try: 
+		regex = r"(![a-zA-Z]+)+"
+		r = re.search(regex, f_read[8])
 		print(r.group(0))
 	except:
 		print("Failed type 1")
-		regex = r"([a-zA-Z]+)+"
-		pass
+		try: 
+			regex = r"([a-zA-Z]+)+"
+			r = re.search(regex, f_read[8])
+			print(r.group(0))
+		except: 
+			print("Failed type 2")
+			format_type = 3 
+			pass
+		else:
+			format_type = 2
 	else: 
 		format_type = 1
-	try: 
-		print(r.group(0))
-	except: 
-		print("Failed type 2")
-		format_type = 3 
-		pass
-	else:
-		format_type = 2
+	
 
 	print("Note: format may be unidentified. Please check POSCAR format and revise code if necessary.")
 	print("Format type:", format_type)
 
 	return format_type, regex
+
+def write_coordinate_labels(i, height, format_type, atom_counter = None, r = None):
+	if format_type == 3:
+		if float(f_read[i].split( )[2]) < height:
+			g.write(f_read[i].strip("\n") + " T T T !" + atomic_species[j] + "\n")
+		elif float(f_read[i].split( )[2]) > height:
+			g.write(f_read[i].strip("\n") + " F F F !" + atomic_species[j] + "\n")
+	else: 	
+		if float(f_read[i].split( )[2]) < height:
+			g.write(f_read[i].replace(r.group(0), "T T T !" + r.group(0)))
+		elif float(f_read[i].split( )[2]) > height:
+			g.write(f_read[i].replace(r.group(0), "F F F !" + r.group(0)))
+		else:
+			pass
 
 def enable_selective_dynamics(format_type, regex):
 	g.write("selective dynamics \n")
@@ -50,19 +60,15 @@ def enable_selective_dynamics(format_type, regex):
 	height = float(input("Freeze below what height (in Angstroms)? "))
 
 	if format_type == 3:
-		# TODO: Fix numbering 
 		j = 0 
 		c = 8 + int(number_of_atoms[j])
 	
 		for i in range(8,len(f_read)):			
 			if i == c-1:
-				print(f_read[i].split( )[2], atomic_species[j])
 				print("i = ", i)
 				print("c = ", c)
-				if float(f_read[i].split( )[2]) < height:
-					g.write(f_read[i].strip("\n") + " T T T !" + atomic_species[j] + "\n")
-				elif float(f_read[i].split( )[2]) > height:
-					g.write(f_read[i].strip("\n") + " F F F !" + atomic_species[j] + "\n")
+				print(f_read[i].split( )[2], atomic_species[j])
+				write_coordinate_labels(i, height, format_type)
 				try:
 					j=j+1
 					c = c + int(number_of_atoms[j])
@@ -71,27 +77,11 @@ def enable_selective_dynamics(format_type, regex):
 					print("end of loop") 
 			else:
 				print(f_read[i].split( )[2], atomic_species[j])
-				print("i = ", i)
-				print("c = ", c)
-				if float(f_read[i].split( )[2]) < height:
-					g.write(f_read[i].strip("\n") + " T T T !" + atomic_species[j] + "\n")
-				elif float(f_read[i].split( )[2]) > height:
-					g.write(f_read[i].strip("\n") + " F F F !" + atomic_species[j] + "\n")
-
-				
-
+				write_coordinate_labels(i, height, format_type, atom_counter=j)
 	else: 
-		try:
-			for i in range(8,len(f_read)):
-				r = re.search(regex, f_read[i])
-				if float(f_read[i].split( )[2]) < height:
-					g.write(f_read[i].replace(r.group(0), "T T T !" + r.group(0)))
-				elif float(f_read[i].split( )[2]) > height:
-					g.write(f_read[i].replace(r.group(0), "F F F !" + r.group(0)))
-				else:
-					pass
-		except: 
-			print("Unidentified format")
+		for i in range(8,len(f_read)):
+			r = re.search(regex, f_read[i])
+			write_coordinate_labels(i, height, format_type, r=r)
 
 ## TO DO: Make modules and classes and shit 
 
--- test.py
+++ test.py
@@ -5,7 +5,24 @@ f_read = f.readlines()
 
 g = open("new_POSCAR", "a+")
 
-regex = r"([a-zA-Z]+)+"
-r = re.search(regex, f_read[8])
-
-print(r.group(0))
+try: 
+	regex = r"(![a-zA-Z]+)+"
+	r = re.search(regex, f_read[8])
+	print(r.group(0))
+except:
+	print("Failed type 1")
+	try: 
+		regex = r"([a-zA-Z]+)+"
+		r = re.search(regex, f_read[8])
+		print(r.group(0))
+	except: 
+		print("Failed type 2")
+		format_type = 3 
+		pass
+	else:
+		format_type = 2
+else: 
+	format_type = 1
+print(f_read[8])
+print(regex)
+print(format_type)
